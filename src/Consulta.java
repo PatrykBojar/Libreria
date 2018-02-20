beb065024a9016db59899c37e23fa4d9a2bb93d5
PatrykBojar <patrickbojar12344@gmail.com>
2018-02-20 15:46:24 +0100

    Creado el recurso para el pool de conexiones

--- src/Consulta.java
+++ src/Consulta.java
@@ -23,14 +23,15 @@ public class Consulta extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-		// Recuperamos el parámetro "autor" del formulario.
+		// Recogemos el parámetro "autor" del formulario.
 		String autor = request.getParameter("autor");
 
-		// Prepramos la salida del servlet.
+		// Preparamos la salida del servlet.
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 
-		// Objetos para la conexión y consulta a la base de datos.
+		// Objetos para la conexión y consulta a la
+		// base de datos.
 		Connection con = null;
 		PreparedStatement stmt = null;
 
@@ -45,23 +46,23 @@ public class Consulta extends HttpServlet {
 
 			// Ejecutamos una consulta SQL.
 			con = DriverManager.getConnection(url, usuario, password);
-			String sql = "SELECT * FROM libros WHERE autor = ?";
-			stmt = con.prepareStatement(sql);
-			stmt.setString(1, "autor");
 
-			// Guardamos los resultados.
+			String sql = "select * from libros where autor = ?";
+			stmt = con.prepareStatement(sql);
+			stmt.setString(1, autor);
+
 			ResultSet resultados = stmt.executeQuery();
 
 			// Mostramos los resultados.
+
 			out.println("<!DOCTYPE html>");
 			out.println("<html>");
 			out.println("<head>");
 			out.println("<meta charset='utf-8'>");
 			out.println("<title>Consulta</title>");
 			out.println("</head>");
-			out.println("<body>");
 			out.println("<h1>Consulta de libros por autor</h1>");
-			out.println("<p>Libros escritos por " + autor + ".</p>");
+			out.println("<p>Libros escritos por " + autor + ":</p>");
 			while (resultados.next()) {
 				out.println("Título: " + resultados.getString("titulo")
 						+ "<br>");
@@ -70,10 +71,10 @@ public class Consulta extends HttpServlet {
 				out.println("Cantidad: " + resultados.getString("cantidad")
 						+ "<br>");
 				out.println("<hr>");
-
 			}
-			out.println("<body>");
-			out.println("<html>");
+			out.println("</body>");
+			out.println("</html>");
+
 		} catch (ClassNotFoundException | SQLException e) {
 			System.out.println("ERROR EN LA CONEXIÓN A LA BASE DE DATOS");
 			e.printStackTrace();
@@ -86,10 +87,12 @@ public class Consulta extends HttpServlet {
 				e.printStackTrace();
 			}
 		}
+
 	}
 
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+
 	}
 
 }
